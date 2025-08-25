a087ddff749cc31b5a2dbb38739729988955ab5a
Aeternuhhm <102520935+vicToriousity@users.noreply.github.com>
2025-08-25 20:11:23 +0930

    ALMOST DONE!!!
    Only issue is getting options canvases to select their specified General button

--- AIE Group Task/Assets/Adamo/Scripts/MenuManager.cs
+++ AIE Group Task/Assets/Adamo/Scripts/MenuManager.cs
@@ -2,80 +2,112 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class MenuManager : MonoBehaviour
 {
+    [Header("Menu Objects")]
     [SerializeField] private GameObject _mainMenuCanvasGO;
     [SerializeField] private GameObject _settingsMenuCanvasGO;
+    [SerializeField] private GameObject _generalMenuCanvasGO;
+    [SerializeField] private GameObject _videoMenuCanvasGO;
+    [SerializeField] private GameObject _audioMenuCanvasGO;
+    [SerializeField] private GameObject _miscMenuCanvasGO;
+
+    [Header("First Selected Buttons")]
+    [SerializeField] private GameObject _mainMenuFirst;
+    [SerializeField] private GameObject _settingsMenuFirst;
+    [SerializeField] private GameObject _generalMenuFirst;
+    [SerializeField] private GameObject _videoMenuFirst;
+    [SerializeField] private GameObject _audioMenuFirst;
+    [SerializeField] private GameObject _miscMenuFirst;
 
     private bool isPaused;
 
     private void Start()
     {
-        _mainMenuCanvasGO.SetActive(false);
+        _mainMenuCanvasGO.SetActive(true);
         _settingsMenuCanvasGO.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(_mainMenuFirst);
     }
 
     private void Update()
     {
-        if (InputManager.instance.MenuOpenCloseInput)
-        {
-            if (!isPaused)
-            {
-                Pause();
-            }
-            else
-            {
-                Unpause();
-            }
-        }
+       
     }
 
     #region Pause/Unpause Functions
-    public void Pause()
-    {
-        isPaused = true;
-        Time.timeScale = 0.0f;
-
-        OpenMainMenu();
-    }
-
-    public void Unpause()
-    {
-        isPaused = false;
-        Time.timeScale = 1.0f;
-
-        CloseAllMenus();
-    }
 
     #endregion
 
     #region Canvas Activations/Deactivations
 
-    private void OpenMainMenu()
+    private void OpenSettingsMenuHandle()
     {
-        _mainMenuCanvasGO.SetActive(true);
         _settingsMenuCanvasGO.SetActive(true);
+        _mainMenuCanvasGO.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(_settingsMenuFirst);
     }
 
-    private void CloseAllMenus()
+    private void CloseSettingsMenu()
     {
-        _mainMenuCanvasGO.SetActive(false);
+        _mainMenuCanvasGO.SetActive(true);
         _settingsMenuCanvasGO.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(_mainMenuFirst);
+    }
+
+    private void OpenGeneralMenu()
+    {
+        EventSystem.current.SetSelectedGameObject(_generalMenuFirst);
+    }
+    private void OpenVideoMenu()
+    {
+        EventSystem.current.SetSelectedGameObject(_videoMenuFirst);
+    }
+    private void OpenAudioMenu()
+    {
+        EventSystem.current.SetSelectedGameObject(_audioMenuFirst);
+    }
+    private void OpenMiscMenu()
+    {
+        EventSystem.current.SetSelectedGameObject(_miscMenuFirst);
     }
 
     #endregion
 
-    #region Meta-commands
+    #region Main Menu Button Actions
 
-    public void Quit() 
+    public void OnSettingsPress()
+    {
+        OpenSettingsMenuHandle();
+    }
+    public void OnPlayPress()
+    {
+        SceneManager.LoadScene("Placeholder");
+    }
+    public void OnQuitPress()
     {
         Application.Quit();
     }
-
-    public void PlayGame()
+    public void OnBackPress()
     {
-        SceneManager.LoadScene("Placeholder");
+        CloseSettingsMenu();
+    }
+    public void OnGeneralPress()
+    {
+        OpenGeneralMenu();
+    }
+    public void OnVideoPress()
+    {
+        OpenVideoMenu();
+    }
+    public void OnAudioPress()
+    {
+        OpenAudioMenu();
+    }
+    public void OnMiscPress()
+    {
+        OpenMiscMenu();
     }
 
     #endregion
