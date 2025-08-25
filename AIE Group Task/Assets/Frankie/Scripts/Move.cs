9b8d2ca9cfbcbe75d566ee60f041c5b14313634a
Scrungleere <frankie.sergi42@gmail.com>
2025-08-25 20:11:03 +0930

    MOVEMENT WORKING !!!
    no rail functionality yet but still yay

--- AIE Group Task/Assets/Frankie/Scripts/Move.cs
+++ AIE Group Task/Assets/Frankie/Scripts/Move.cs
@@ -4,77 +4,76 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    private float horizontal;
-    private float jumppower = 4f;
-    private float passivemovement = 6f;
-    private float speedstart = 9f;
-    private float speedlean = 4f;
-    public float horizontalVelocity;
-    private bool isGrounded;
+    
+    
+    public float passivemovement = 6f;
+    public float speedstart = 9f;
+    public float speedlean = 4f;
+    public float leanLimit = 6f;
+    public float upperLimit = 10f;
+  
     private bool oneSecond = true;
-
-
-
+        
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float horizontalVelocity;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+  
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
-
-        
         horizontalVelocity = rb.velocity.x;
-       
 
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) && IsGrounded())
         {
-                    
-            
+
+
             //transform.Translate(Vector2.right * Time.deltaTime * speedstart);
-            rb.velocity = new Vector2(passivemovement, rb.velocity.y);  //this is setting velocity when it gets to it, invalidating  the rest
-            if(horizontalVelocity <=15f && oneSecond==true)
+            if (horizontalVelocity < passivemovement) 
             {
-                Debug.Log("regular");
+                rb.AddForce(transform.right * passivemovement);  
+                //Debug.Log(horizontalVelocity);
+            }
+
+            if(horizontalVelocity <=leanLimit && oneSecond==true)
+            {
+                
                 StartCoroutine("Smallpush");
                 StartCoroutine("Secondtimer");
             }
-            else if (oneSecond==true) 
+            else if (horizontalVelocity <= upperLimit && oneSecond ==true) 
             {
-                Debug.Log("speedlean");
-                rb.velocity = new Vector2(speedlean, rb.velocity.y);
+                //Debug.Log("speedlean");
+                rb.AddForce(transform.right * speedlean);
             }
            
-            //rb.velocity replaces speed value, so need to find a way that works to create a baseline of speed hile adding AND NOT REPLACEING
-            //speed when going back to the start of void update
+           
             
         }
 
-        if (Input.GetKeyDown(KeyCode.S) && IsGrounded())
-        {
-            
-            rb.velocity = new Vector2(rb.velocity.x, jumppower);
-        }
+        
 
     }
+
     private bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
         //you slam ur penis imn the car door
     }
 
-
     IEnumerator Smallpush ()
     {
-        Debug.Log("startcorotine");
-        rb.velocity = new Vector2(speedstart, rb.velocity.y);
-        yield return new WaitForSeconds(3);
+        //Debug.Log("startcorotine");
+        rb.AddForce(transform.right * speedstart, ForceMode2D.Impulse);
+        yield return new WaitForSeconds(1);
         
         
     }
@@ -82,8 +81,8 @@ public class Move : MonoBehaviour
     IEnumerator Secondtimer ()
     {
         oneSecond = false;
-        Debug.Log("timerstart");
-        yield return new WaitForSeconds(3);
+        //Debug.Log("timerstart");
+        yield return new WaitForSeconds(1);
         oneSecond = true;
     }
 
---
+++ AIE Group Task/Assets/Frankie/Scripts/RailStick.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class RailStick : MonoBehaviour
+{
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    //this script
+    //create new tag for the rail
+    //if player tag enters collider, trigger global bool
+    //when enters hitbox, follows path with speed told from the player movement script
+
+    //also allow option for early exit that gives a slightly higher jump
+
+    //also code in tricks
+
+
+    //Player movemnt script
+    //record velocity when bool swithces, it will be the speed as u are going while on it and when u leave it
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
