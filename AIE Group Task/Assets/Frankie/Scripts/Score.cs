2faf72d9fc9e4efb6e1a354e0db431772ee41543
Scrungleere <frankie.sergi42@gmail.com>
2025-08-27 20:14:59 +0930

    lol i did like nothing but post merge

---
+++ AIE Group Task/Assets/Frankie/Scripts/Score.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    [SerializeField] public int score;
+    [SerializeField] public int tl;
+    [SerializeField] public int s;
+
+    public int spamCount;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
