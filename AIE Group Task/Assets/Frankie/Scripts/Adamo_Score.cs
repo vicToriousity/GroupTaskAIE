3a78d080ee75a82c842ce6ddb806697c77f61017
Aeternuhhm <102520935+vicToriousity@users.noreply.github.com>
2025-09-01 19:01:53 +0930

    SCORE SYSTEM IS DO A WORKINGS!
    YIPPEE!

--- AIE Group Task/Assets/Frankie/Scripts/Adamo_Score.cs
+++ AIE Group Task/Assets/Frankie/Scripts/Adamo_Score.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Adamo_Score : MonoBehaviour
 {
+    public Move moveScriptReference;
+    public TMP_Text speedText;
+    public TMP_Text scoreText;
+    public int TrickCount = 0;
+    public float ComboScore = 0;
+    public float Score = 0;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,21 @@ public class Adamo_Score : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        speedText.text = ("Speed: " + (moveScriptReference.horizontalVelocity.ToString("F2")));
+
+        ComboScore = (TrickCount * moveScriptReference.horizontalVelocity);
+
+
+        if (Input.GetKeyDown(KeyCode.A) && moveScriptReference.IsGrounded() == false)
+        {
+            TrickCount++;
+        }
+
+        if (moveScriptReference.IsGrounded())
+        {
+            TrickCount = 0;
+            Score = Score + ComboScore;
+            scoreText.text = ("Score: " + Score.ToString("F0"));
+        }
     }
 }
