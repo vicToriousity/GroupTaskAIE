3ec6fc30d35503724a376b2573b1ac8559409470
Aeternuhhm <102520935+vicToriousity@users.noreply.github.com>
2025-09-01 20:10:48 +0930

    ABSOLUTE W

--- AIE Group Task/Assets/Frankie/Scripts/Adamo_Score.cs
+++ AIE Group Task/Assets/Frankie/Scripts/Adamo_Score.cs
@@ -11,6 +11,7 @@ public class Adamo_Score : MonoBehaviour
     public int TrickCount = 0;
     public float ComboScore = 0;
     public float Score = 0;
+    public float finalTrickCounter = 0;
 
 
 
@@ -23,9 +24,34 @@ public class Adamo_Score : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (TrickCount == 0)
+        {
+            finalTrickCounter = 0;
+        }
+
+        else if (TrickCount >= 1 & TrickCount < 2)
+        {
+            finalTrickCounter = 1;
+        }
+
+        else if (TrickCount >= 2 & TrickCount < 4)
+        {
+            finalTrickCounter = 2;
+        }
+
+        else if (TrickCount >= 4 & TrickCount < 6)
+        {
+            finalTrickCounter = 4;
+        }
+
+        else
+        {
+            finalTrickCounter = 16;
+        }
+
         speedText.text = ("Speed: " + (moveScriptReference.horizontalVelocity.ToString("F2")));
 
-        ComboScore = (TrickCount * moveScriptReference.horizontalVelocity);
+        ComboScore = ((TrickCount * finalTrickCounter) * moveScriptReference.horizontalVelocity);
 
 
         if (Input.GetKeyDown(KeyCode.A) && moveScriptReference.IsGrounded() == false)
@@ -35,6 +61,7 @@ public class Adamo_Score : MonoBehaviour
 
         if (moveScriptReference.IsGrounded())
         {
+
             TrickCount = 0;
             Score = Score + ComboScore;
             scoreText.text = ("Score: " + Score.ToString("F0"));
